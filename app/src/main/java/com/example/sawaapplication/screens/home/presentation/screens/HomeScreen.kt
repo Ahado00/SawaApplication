92a9d468003c3a15dab2e00728d212cbfc9d3220
etharalrehaili <85852139+etharalrehaili@users.noreply.github.com>
2025-05-18 21:58:25 +0300

    mvvm for notification + fix some issues

--- app/src/main/java/com/example/sawaapplication/screens/home/presentation/screens/HomeScreen.kt
+++ app/src/main/java/com/example/sawaapplication/screens/home/presentation/screens/HomeScreen.kt
@@ -91,7 +91,7 @@ fun PostsTab(viewModel: HomeViewModel, navController: NavController) {
         postLikedUserId?.let { likedUserId ->
             val post = posts.find { it.userId == likedUserId }
             post?.let {
-                notificationViewModel.sendLikeNotification(it)
+                notificationViewModel.notifyLike(it)
             }
         }
     }
@@ -134,7 +134,7 @@ fun PostsTab(viewModel: HomeViewModel, navController: NavController) {
                             onClick = {},
                             onLikeClick = {
                                 viewModel.likePost(post)
-                                notificationViewModel.sendLikeNotification(post)
+                                notificationViewModel.notifyLike(post)
                             },
                             navController = navController,
                             onUserImageClick = { viewModel.likePost(post) }
