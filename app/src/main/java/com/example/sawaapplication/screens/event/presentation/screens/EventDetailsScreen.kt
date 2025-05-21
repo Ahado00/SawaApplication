3955d554102e68ef27286416650c8ab3d3bf88ea
Shahad Ali Aldawsari <shahodah.ali@gmail.com>
2025-05-21 08:57:22 +0300

    CA-195-Added the type field in the NewCommunity form

--- app/src/main/java/com/example/sawaapplication/screens/event/presentation/screens/EventDetailsScreen.kt
+++ app/src/main/java/com/example/sawaapplication/screens/event/presentation/screens/EventDetailsScreen.kt
@@ -1,8 +1,11 @@
 package com.example.sawaapplication.screens.event.presentation.screens
+
 import android.content.Intent
 import android.net.Uri
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -10,11 +13,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.integerResource
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat.startActivity
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
 import com.example.sawaapplication.screens.home.presentation.vmModels.HomeViewModel
 
 @Composable
@@ -22,7 +30,7 @@ fun EventDetailScreen(
     communityId: String,
     eventId: String,
     navController: NavController,
-    viewModel: HomeViewModel = hiltViewModel()
+    viewModel: HomeViewModel = hiltViewModel(),
 ) {
 
     LaunchedEffect(Unit) {
@@ -49,29 +57,39 @@ fun EventDetailScreen(
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        LazyColumn {
-            item {
-                Text(text = event.title, style = MaterialTheme.typography.headlineMedium)
-                Text(text = event.description, style = MaterialTheme.typography.bodyMedium)
-                Text(text = "Date: ${event.date}", style = MaterialTheme.typography.bodySmall)
+        Column {
 
-                Spacer(modifier = Modifier.height(16.dp))
+            Text(text = event.title, style = MaterialTheme.typography.headlineMedium)
 
-                Button(onClick = {
-                    val geoUri = Uri.parse("google.navigation:q=${event.latitude},${event.longitude}")
-                    val intent = Intent(Intent.ACTION_VIEW, geoUri).apply {
-                        setPackage("com.google.android.apps.maps")
-                    }
-                    startActivity(context, intent, null)
-                }) {
-                    Text("Provide Location")
+            //I need to add event image
+            Image(
+                painter = rememberAsyncImagePainter(model = event.imageUri),
+                contentDescription = null,
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(16.dp))
+            )
+
+            Text(text = event.description, style = MaterialTheme.typography.bodyMedium)
+            Text(text = "Date: ${event.date}", style = MaterialTheme.typography.bodySmall)
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = {
+                val geoUri = Uri.parse("google.navigation:q=${event.latitude},${event.longitude}")
+                val intent = Intent(Intent.ACTION_VIEW, geoUri).apply {
+                    setPackage("com.google.android.apps.maps")
                 }
+                startActivity(context, intent, null)
+            }) {
+                Text("Provide Location")
+            }
 
-                Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
-                Button(onClick = { navController.popBackStack() }) {
-                    Text("Back")
-                }
+            // I need to change the back button
+            Button(onClick = { navController.popBackStack() }) {
+                Text("Back")
             }
         }
     }
